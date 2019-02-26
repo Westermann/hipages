7020eac3f501531e22749f8e3e6ba539da5b7e9c
Westermann <westermann93@googlemail.com>
2019-02-26 20:20:35 +1100

    :one: First table produced

--- src/main/scala/Etl.scala
+++ src/main/scala/Etl.scala
@@ -1,9 +1,9 @@
 package com.hipages.hitest
 
 
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import scala.util.Properties
-import scala.util.parsing.json.{JSON, JSONObject}
 
 
 object Etl {
@@ -20,7 +20,7 @@ object Etl {
       .getOrCreate()
 
     val df = loadJson
-    println(df.show())
+    println(createUserEventTable(df).show())
 
     spark.stop
   }
@@ -28,7 +28,37 @@ object Etl {
   def loadJson(implicit spark: SparkSession): DataFrame = {
     import spark.implicits._
     val jsonLines = spark.sparkContext.textFile(rawJsonFilePath)
-    jsonLines.foreach(println)
-    spark.read.json(jsonLines)
+    val jsonDf = spark.read.json(jsonLines)
+      .withColumn("user_id", $"user.id")
+      .withColumn("user_session_id", $"user.session_id")
+      .withColumn("user_ip", $"user.ip")
+    jsonDf
+  }
+
+  def createUserEventTable(df: DataFrame)(implicit spark: SparkSession): DataFrame = {
+    val extractUrlPartsUdf = udf(extractUrlParts(_))
+    df
+      .withColumn("url_split", extractUrlPartsUdf(col("url")))
+      .withColumn("url_level1", col("url_split").getItem(0))
+      .withColumn("url_level2", col("url_split").getItem(1))
+      .withColumn("url_level3", col("url_split").getItem(2))
+      .drop(col("url_split"))
+      .drop(col("url"))
+      .withColumnRenamed("action", "activity")
+      .withColumnRenamed("timestamp", "time_stamp")
+      .select("user_id", "time_stamp", "url_level1", "url_level2", "url_level3", "activity")
+  }
+
+  def extractUrlParts(url: String): Seq[String] = {
+    val pattern = 
+      """([^\/]+:\/\/|)
+        |([a-zA-Z]*\.[a-zA-Z.]*)[\/]*
+        |([a-zA-z0-9%]*)[\/]*
+        |([a-zA-z0-9%]*)[A-Za-z0-9-._~:/?#\[\]@!$&'()*+,;=]*
+        |""".stripMargin.replaceAll("\n","").r
+    url match {
+        case pattern(protocol, domain, first, second) => Seq(domain, first, second)
+        case _ => Seq("", "", "")
+    }
   }
 }
--- src/test/scala/EtlSpec.scala
+++ src/test/scala/EtlSpec.scala
@@ -6,7 +6,7 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.scalatest._
 
 
-class EtlSpec extends FlatSpec with BeforeAndAfterAll {
+class EtlSpec extends FlatSpec with BeforeAndAfterAll with Matchers {
 
   val etl = Etl 
   import etl._
@@ -15,11 +15,33 @@ class EtlSpec extends FlatSpec with BeforeAndAfterAll {
     .config("spark.master", "local[*]")
     .getOrCreate()
 
+  "The Etl object extractURLParts function" should "correctly extract url parts" in {
+    Etl.extractUrlParts("https://www.hipages.com.au/first/second") shouldEqual Seq("www.hipages.com.au", "first", "second")
+  }
+
+  it should "correctly extract only one url part" in {
+    Etl.extractUrlParts("https://www.hipages.com.au/") shouldEqual Seq("www.hipages.com.au", "", "")
+  }
+
+  it should "correctly extract from url without protocoll" in {
+    Etl.extractUrlParts("hipages.com.au/") shouldEqual Seq("hipages.com.au", "", "")
+  }
+ 
+  it should "correctly extract no url from single word" in {
+    Etl.extractUrlParts("invalid") shouldEqual Seq("", "", "")
+  }
+ 
+  it should "correctly extract from url with query" in {
+    Etl.extractUrlParts("hipages.com/?asdf=5&h=6") shouldEqual Seq("hipages.com", "", "")
+  }
+
   "The Etl object" should "load the Json data correctly" in {
     Etl.loadJson
   }
 
-  override def afterAll {
-    spark.stop
+  it should "correctly generate a user event table" in {
+    val df = Etl.loadJson
+    Etl.createUserEventTable(df)
   }
+
 }
